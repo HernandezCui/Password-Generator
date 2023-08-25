c3e04f2ab5299b5c5535787e7c3bdcbb215caa24
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-08-24 20:56:53 -0700

    revised password Characters & Criteria prompts

--- script.js
+++ script.js
@@ -3,23 +3,54 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
+// Password Characters 
+  var passwordOptions = { 
+    numeric: "0123456789",
+    specialChar: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    lowerCaseABC: "abcdefghijklmnopqrstuvwxyz",
+    upperCaseABC: "ABCDEFGHIJKLMNOPQRSTUVVWXYZ"
+  }
+
+  var passInfo = "";
+  var passChars = [];
+
   // criteria prompts for password
-  var keylength = prompt("Password must be between 8 and 128 characters! Click OK to continue"); 
-  var numbers = confirm("Your password needs a number! Click OK to continue");
-  var symbols = confirm("Your password needs a symbol! Click OK to continue");
-  var uppercase = confirm("Your password needs an uppercase letter! Click OK to continue");
-  var lowercase = confirm("Your password needs a lowercase letter! Click OK to continue");
+  var characterLength = prompt("In order to generate a password, you must enter the length of characters you desire that are in between 8 and 128 characters!");
+    if (characterLength < 8 && characterLength > 128) {
+      alert("Character length does not match requirements");
+    }
+
+  var getNumber = confirm("Do you want to include NUMBERS?");
+    if (getNumber) {
+      passInfo += passwordOptions.numeric;
+      passChars.push(getRandomChar(passwordOptions.numeric));
+    }
+
+  var getSpecialCharacters = confirm("Do you want to include SPECIAL CHARACTERS?");
+    if (getSpecialCharacters) {
+      passInfo += passwordOptions.specialChar;
+      passChars.push(getRandomChar(passwordOptions.specialChar));
+    }
+
+  var getLowerCase = confirm("Do you want to include LOWERCASE characters?");
+    if (getLowerCase) {
+      passInfo += passwordOptions.lowerCaseABC;
+      passChars.push(getRandomChar(passwordOptions.lowerCaseABC));
+    }
+      
+  var getUpperCase = confirm("Do you want to include UPPERCASE characters?");
+      if (getUpperCase) {
+          passInfo += passwordOptions.upperCaseABC;
+          passChars.push(getRandomChar(passwordOptions.upperCaseABC));
+        }
+  } 
 
 
-  // Password Characters
-  var numeric ="0123456789";
-  var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
-  var specialSymbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var multiselect = "[]"; 
-}
 
 
+
+
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
