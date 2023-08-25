f75852c5a841cf898001b71020b4eebf65d8973b
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-08-24 23:30:39 -0700

    fixed password characters & crtieria prompts

--- script.js
+++ script.js
@@ -1,56 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
 
-// Password Characters 
-  var passwordOptions = { 
-    numeric: "0123456789",
-    specialChar: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-    lowerCaseABC: "abcdefghijklmnopqrstuvwxyz",
-    upperCaseABC: "ABCDEFGHIJKLMNOPQRSTUVVWXYZ"
-  }
-
-  var passInfo = "";
-  var passChars = [];
-
-  // criteria prompts for password
-  var characterLength = prompt("In order to generate a password, you must enter the length of characters you desire that are in between 8 and 128 characters!");
-    if (characterLength < 8 && characterLength > 128) {
-      alert("Character length does not match requirements");
-    }
-
-  var getNumber = confirm("Do you want to include NUMBERS?");
-    if (getNumber) {
-      passInfo += passwordOptions.numeric;
-      passChars.push(getRandomChar(passwordOptions.numeric));
-    }
-
-  var getSpecialCharacters = confirm("Do you want to include SPECIAL CHARACTERS?");
-    if (getSpecialCharacters) {
-      passInfo += passwordOptions.specialChar;
-      passChars.push(getRandomChar(passwordOptions.specialChar));
-    }
-
-  var getLowerCase = confirm("Do you want to include LOWERCASE characters?");
-    if (getLowerCase) {
-      passInfo += passwordOptions.lowerCaseABC;
-      passChars.push(getRandomChar(passwordOptions.lowerCaseABC));
-    }
-      
-  var getUpperCase = confirm("Do you want to include UPPERCASE characters?");
-      if (getUpperCase) {
-          passInfo += passwordOptions.upperCaseABC;
-          passChars.push(getRandomChar(passwordOptions.upperCaseABC));
-        }
-  } 
-
-
-
-
-
-
-  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -62,3 +13,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+
+  // Password Characters 
+    var numericChar = "0123456789";
+    var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+    var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVVWXYZ";
+
+  
+  //  Password length
+    var characterLength = prompt("In order to generate a password, you must enter the length of characters you desire that are in between 8 and 128 characters!");
+      if (characterLength < 8 || characterLength > 128) {
+        alert("Character length does not match requirements");
+      }
+  
+// criteria prompts 
+    var getNumber = confirm("Do you want to include NUMBERS?");
+    var getSpecialCharacters = confirm("Do you want to include SPECIAL CHARACTERS?");
+    var getLowerCase = confirm("Do you want to include LOWERCASE characters?");
+    var getUpperCase = confirm("Do you want to include UPPERCASE characters?");
+  
+    // validate one character type
+      if (!getNumber || !getSpecialCharacters || !getLowerCase || !getUpperCase) {
+        alert ("Must select at least one type of character")
+       }
+  
+
+
+
+    
+      
+    } 
