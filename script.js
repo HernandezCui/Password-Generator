327e128e78fe18a860967b9bdf79b018899099ef
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-08-24 17:04:53 -0700

    Created the criteria prompts

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  // criteria prompts for password
+  var keylength = prompt("Password must be between 8 and 128 characters! Click OK to continue"); 
+  var numbers = confirm("Your password needs a number! Click OK to continue");
+  var symbols = confirm("Your password needs a symbol! Click OK to continue");
+  var uppercase = confirm("Your password needs an uppercase letter! Click OK to continue");
+  var lowercase = confirm("Your password needs a lowercase letter! Click OK to continue");
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
